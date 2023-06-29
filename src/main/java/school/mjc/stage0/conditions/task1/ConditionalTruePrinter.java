bdae31c1358592761393635ce2355a9cd02a69f0
Ilucha <pozta_01@mail.ru>
2023-06-29 14:35:14 +0300

    if

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        boolean c = true;
+        if(c){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
     }
+
 }
